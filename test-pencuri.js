ed3fbda244884a139016534085f982db1a722699
ziqfortune <ziqfortune@gmail.com>
2026-09-14 23:43:53 +0800

    Add files via upload

--- test-pencuri.js
+++ test-pencuri.js
@@ -2,9 +2,9 @@ const { getStreams } = require('./providers/pencurimovie.js');
 
 async function test() {
   console.log('Testing PencuriMovie provider...');
-  // TMDB ID 872585 = Oppenheimer
-  const results = await getStreams('872585', 'movie');
-  console.log('Stream jumpa:', results);
+  // ID 1472152 = Balas Balik
+  const results = await getStreams('1472152', 'movie');
+  console.log('Hasil Stream:', results);
 }
 
 test();
